33fb737ac93cd4cfbccff758b25d9898caf2610f
sebas2001betancur <sebas2001betancur@gmail.com>
2024-01-14 23:19:15 -0500

    Primer commit

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className="space-y-3">
+        <p className="font-semibold">Sebastian Betancur</p>
+        <p>Envigado, antioquia, 98983</p>
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>3003560070</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddressCard;
---
+++ src/customer/components/HomeCarosel/MainCarosel.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { MainCaroselData } from "./MainCaroselData";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const MainCarosel = () => {
+  const items = MainCaroselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      role="presentation"
+      src={item.image}
+      alt=""
+    />
+  ));
+  return (
+    <AliceCarousel
+      items={items}
+      disableButtonsControls
+      autoPlay
+      autoPlayInterval={1000}
+      infinite
+    />
+  );
+};
+
+export default MainCarosel;
